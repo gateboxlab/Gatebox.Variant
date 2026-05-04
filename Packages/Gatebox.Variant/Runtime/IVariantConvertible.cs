52aa4922801660bcb48cbd6899a07451e1192afc
Nishie Minoru <nishie.minoru@gateboxlab.com>
2026-05-04 14:23:42 +0900

    using 整理、改行コードの統一

--- Packages/Gatebox.Variant/Runtime/IVariantConvertible.cs
+++ Packages/Gatebox.Variant/Runtime/IVariantConvertible.cs
@@ -1,13 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 #nullable enable
 
 namespace Gatebox.Variant
 {
+	/// <summary>
+	/// JVaraint との互換性を持つ型
+	/// <para>
+	/// このインターフェイスを実装することで、型は JVariant との相互変換が可能になります。
+	/// AsVariant() を実装し、 JVariant を受けるコンストラクタを定義してください。</para>
+	/// </summary>
 	public interface IVariantConvertible
 	{
 		public JVariant AsVariant();
